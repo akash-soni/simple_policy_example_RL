d3552dfa01200e58885b8349304ad1ab772ce9c8
akash <akash.200287@gmail.com>
2022-07-09 18:35:37 +0530

    simple policy without neural networks

--- src/simple_policy_example.py
+++ src/simple_policy_example.py
@@ -3,6 +3,7 @@ from turtle import done
 import gym
 import numpy as np
 import time
+from tqdm import tqdm
 
 env = gym.make("CartPole-v1")
 
@@ -30,25 +31,22 @@ N_steps = 200
 
 
 # simple render for 100 episodes
-for episode in range(N_episodes):
+for episode in range(N_episodes): # Running for 200 episodes and for each episode we have 200 steps/trials 
     rewards = 0
     # CartPosition, CartVelocity, PoleAngle, PoleAngularVelocity
-    Observations = env.reset() # initial positions
+    Observations = env.reset() # the pole will be somewhere in the middle on initial positions
     PoleAngle = Observations[2] 
-    for step in range(N_steps):
+    for step in tqdm(range(N_steps)): 
         env.render()
-        action = basic_policy(PoleAngle)
-        Observations, reward, done, info = env.step(action)
+        action = basic_policy(PoleAngle) # if the pole is moving to right move to right and if pole is moving to left move to left
+        Observations, reward, done, info = env.step(action) # after the movement we will  get the information
         time.sleep(0.001)
         rewards += reward
-        if done: #Fallen
+        if done: #Fallen 
             break
     total_reward.append(rewards)
 
-stats = {
-    "mean": np.mean(total_reward)
-    "std": np.
-}
+
 
 
 
